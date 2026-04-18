c56d056fd1f5a4321a429d4200ce9bf1777888b9
Jdjdjdjdj17 <jupasandoval17@gmail.com>
2026-04-18 15:21:23 -0600

    Grafo y usc

---
+++ Enfoque1/BusquedaNoInformada/_2_BusquedaEnAnchuraCostoUniforme.py
@@ -0,0 +1,36 @@
+import heapq
+from grafo_datos import GRAFO, INICIO, DESTINO
+
+print(f"Grafo cargado: {GRAFO}  Inicio: {INICIO}  |  Destino: {DESTINO}")
+
+def ucs(grafo, inicio, destino):
+    cola = [(0, [inicio])]    # ← inicio adentro
+    visitado = set()
+
+    while cola:               # ← adentro de la función
+        costo, camino = heapq.heappop(cola)
+        nodo = camino[-1]
+
+        if nodo in visitado:
+            continue
+        visitado.add(nodo)    # ← add no append
+
+        if nodo == destino:
+            print("Costo total: ", costo)
+            return camino     # ← camino no cola
+
+        for vecino, peso in grafo[nodo]:
+            if vecino not in visitado:
+                nuevo_costo = costo + peso
+                heapq.heappush(cola, (nuevo_costo, camino + [vecino]))
+
+    return None               # ← N mayúscula
+
+camino = ucs(GRAFO, INICIO, DESTINO)
+
+if camino:
+    print("El camino mas barato es:", camino)
+else:
+    print("No se encontró camino.")
+
+print("Este espacio es para el algoritmo de Busqueda en anchura de costo uniforme")
